97d5280e779d1fda53aa2c55f4b0a0d390d88ccd
Artur <61884137+Dathator@users.noreply.github.com>
2021-12-18 11:27:38 +0300

    2

--- Assets/Models/Stylized Astronaut/Character/Player.cs
+++ Assets/Models/Stylized Astronaut/Character/Player.cs
@@ -27,8 +27,6 @@ public class Player : MonoBehaviour {
 				moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
 			}
 
-			float turn = Input.GetAxis("Horizontal");
-			transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
 			controller.Move(moveDirection * Time.deltaTime);
 			moveDirection.y -= gravity * Time.deltaTime;
 		}
